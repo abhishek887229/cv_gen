822fd3a95474efd0807a21a1d971e8500d140a37
abhishek887229 <techiot990@gmail.com>
2023-11-19 18:51:24 +0530

    build delete,view,downalod fuction till now

--- pdf/urls.py
+++ pdf/urls.py
@@ -6,7 +6,10 @@ app_name="cv"
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("",views.accept,name="data"),
+    path("",views.home,name="home"),
+    path("generate/",views.accept,name="data"),
     path("<int:id>/",views.resume,name="resume"),
     path("list/",views.get_list,name="data_list"),
+    path("del/<int:id>/",views.delete_data,name="delete"),
+    path("view/<int:id>/",views.view_cv,name="view"),
 ]
--- pdf/views.py
+++ pdf/views.py
@@ -1,11 +1,15 @@
 # views.py
 
-from django.shortcuts import render
+from django.shortcuts import render,redirect,get_object_or_404
 from django.http import HttpResponse
 from django.template import loader
 from .models import profile  # Adjusted model name to follow Python conventions
 import pdfkit
 import io
+
+
+def home(request):
+    return render(request,'pdf/home.html')
 
 def accept(request):
     if request.method == 'POST':
@@ -41,10 +45,15 @@ def accept(request):
 
 #we get a specific user id and convert the webpage into the pdf so user can downlaod it as required.
 
+from django.http import HttpResponse
+from django.template import loader
+from .models import profile  # Import your 'profile' model
+import pdfkit
+
 def resume(request, id):
     user_profile = profile.objects.get(pk=id)
     template = loader.get_template('pdf/get_cv.html')
-    html = template.render({'user_profile': user_profile})  
+    html = template.render({'user_profile': user_profile})
 
     options = {
         'page-size': 'Letter',
@@ -52,10 +61,15 @@ def resume(request, id):
     }
 
     pdf = pdfkit.from_string(html, False, options)
+
+    # Set the filename based on the 'name' column in the database
+    filename = f"{user_profile.name}_{user_profile.id}_resume.pdf"
+
     response = HttpResponse(pdf, content_type='application/pdf')
-    response['Content-Disposition'] = 'attachment; filename="resume.pdf"'  # Fix this line
+    response['Content-Disposition'] = f'attachment; filename="{filename}"'
 
     return response
+
 
 
 def get_list(request):
@@ -63,3 +77,14 @@ def get_list(request):
     list=profile.objects.all()
 
     return render(request,'pdf/all_list.html',{'all_data':list})
+
+def delete_data(request,id):
+    delete_list=profile.objects.get(pk=id)
+    delete_list.delete()
+
+    return redirect("cv:data_list")
+
+
+def view_cv(request, id):
+    view_data = get_object_or_404(profile, pk=id)  # Use get_object_or_404 to handle the case when the object is not found
+    return render(request, 'pdf/view_cv.html', {'view': [view_data]})
